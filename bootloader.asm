7c2585d513066a857a8b9277b0e7b483c7faeee0
SorerBOT <alonfiller@gmail.com>
2026-01-24 15:17:57 +0200

    FEAT defined dap

--- bootloader.asm
+++ bootloader.asm
@@ -6,18 +6,31 @@
 ;           terminate with the magic number: 0x55AA (big endian).
 ;   - BIOS copy these 512 bytes to address 0x7c00 and JMP to it
 ;   - This is where our bootloader comes to life.
+[BITS 16]                 ; setting 16-bit mode
+[ORG 0x7C00]
 
-BITS 16                 ; 16-bit mode
-ORG 0x7C00
+load_stage_2:
+                        ; BIOS store the current boot driveID in dl. We need to set it ourselves.
+    mov si, DAP
+    mov ah, 0x42
+    int 0x13
+    jc short .error
 
-dw 0x1111
+; Defining DAP for BIOS Interrupt 13h which reads from disk and writes to RAM
+align 4                 ; Just to be safe, align on 4-byte boundary
+DAP:
+    db 0x10             ; Packet Size, this tells the BIOS what version of DAP struct we're using
+    db 0x00             ; Padding byte. Needs to be reset to 0 if ran in a loop
+    dw 0x4000           ; [2] Count: Number of sectors to read (1 sector)
+
+                        ; RAM address to write to is represented by (Segment * 16) + Offset
+    dw 0x1000           ; Offset
+    dw 0x0000           ; Segment
+
+    dq 0x00000001       ; Disk sector to read from, each sector is 512 bytes.
+                        ; the first one contains this file, the next sector will contain stage 2.
 
 
-_start:
-call 0x5
-move rax, [0x3]
-
-; Magic Number at the end of the 512 bytes indicates that this is a bootloader
+; Adding the magic number at the end of the 512 bytes indicates that this is a bootloader
 times 510-($-$$) db 0
-
 dw 0xAA55
