2f93a4d74258249fb51c3fbbc06686cc3c85b950
SorerBOT <alonfiller@gmail.com>
2026-01-24 15:25:55 +0200

    FEAT now jumping to stage 2, concluding stage 1

--- bootloader.asm
+++ bootloader.asm
@@ -6,15 +6,25 @@
 ;           terminate with the magic number: 0x55AA (big endian).
 ;   - BIOS copy these 512 bytes to address 0x7c00 and JMP to it
 ;   - This is where our bootloader comes to life.
-[BITS 16]                 ; setting 16-bit mode
+[BITS 16]               ; setting 16-bit mode
 [ORG 0x7C00]
 
+start:
+    jmp load_stage_2
+
+
 load_stage_2:
-                        ; BIOS store the current boot driveID in dl. We need to set it ourselves.
+                        ; BIOS store the current boot driveID in dl. We need not set it ourselves.
     mov si, DAP
     mov ah, 0x42
     int 0x13
-    jc short .error
+    jc error            ; CF==1 means that an error has occurred.
+    jmp 0x1000          ; jump to the RAM address where we loaded the code
+
+error:
+    jmp $               ; infinite loop. I use it to keep QEMU open
+
+
 
 ; Defining DAP for BIOS Interrupt 13h which reads from disk and writes to RAM
 align 4                 ; Just to be safe, align on 4-byte boundary
@@ -29,6 +39,9 @@ DAP:
 
     dq 0x00000001       ; Disk sector to read from, each sector is 512 bytes.
                         ; the first one contains this file, the next sector will contain stage 2.
+
+
+
 
 
 ; Adding the magic number at the end of the 512 bytes indicates that this is a bootloader
